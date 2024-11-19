c1c94262518b0cf8aa6a584660a914cf90bc4431
Luk?? Gereg <lukas.gereg@student.tuke.sk>
2024-11-19 09:56:14 +0000

    adding cross validation to main pipeline

--- models/combined_vvg_model.py
+++ models/combined_vvg_model.py
@@ -38,8 +38,12 @@ class CombinedVVGModel(BaseModel):
         x = torch.cat((x1, x2), dim=1)
 
         x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
         x = self.dropout(x)
-        x = self.fc4(self.fc3(self.fc2(x)))
+
+        x = self.fc4(x)
 
         return x
 
